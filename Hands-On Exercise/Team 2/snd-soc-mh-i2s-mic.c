4977fcbe00388b75bc35d26552c91d4028aab997
Victor Martinez <victor.a.martinez@capgemini.com>
2025-08-20 19:29:26 -0600

    Team 2: Victor M add mic on DTS and merge on a single card

--- Hands-On Exercise/Team 2/snd-soc-mh-i2s-mic.c
+++ Hands-On Exercise/Team 2/snd-soc-mh-i2s-mic.c
@@ -16,25 +16,25 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-// Module metadata
+/* Module metadata */
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Victor Martinez");
 MODULE_DESCRIPTION("mic driver  for INMP411");
 MODULE_VERSION("1.0");
 
-// Module initialization function
+/* Module initialization function */
 static int __init hello_init(void)
 {
     printk(KERN_INFO "Hello: Module loaded successfully\n");
     return 0; // Return 0 for success
 }
 
-// Module cleanup function
+/* Module cleanup function */
 static void __exit hello_exit(void)
 {
     printk(KERN_INFO "Hello: Module unloaded successfully\n");
 }
 
-// Register init and exit functions
+/*  Register init and exit functions */
 module_init(hello_init);
 module_exit(hello_exit);
